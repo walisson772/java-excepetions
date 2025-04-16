955ca4b75ef801de4a9d70b156a41813da497e66
Walisson Santos <walissonsantost008@gmail.com>
2025-04-15 22:14:58 -0300

    Falta apenas algumas refatorações para deixar o codigo de mais facil compreensão

--- untitled/src/aplication/Program.java
+++ untitled/src/aplication/Program.java
@@ -13,12 +13,12 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        System.out.print("Room number: ");
-        int roomNumber = sc.nextInt();
-        System.out.print("Check-in date (dd/MM/yyyy): ");
-        Date checkin = sdf.parse(sc.next());
-        System.out.print("Check-out date (dd/MM/yyyy): ");
-        Date checkout = sdf.parse(sc.next());
+            System.out.print("Room number: ");
+            int roomNumber = sc.nextInt();
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            Date checkin = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            Date checkout = sdf.parse(sc.next());
 
         if (!checkout.after(checkin)){
             System.out.println("Error in reservation: Check-out date must be after check-in date");
@@ -26,6 +26,23 @@ public class Program {
         else{
             Reservation reservation = new Reservation(roomNumber, checkin, checkout);
             System.out.println("Reservation: "+reservation);
+
+            System.out.println("Enter date to update the reservation:");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkin = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkout = sdf.parse(sc.next());
+
+            Date now = new Date();
+            if (checkin.before(now) || checkout.before(now)){
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            }else if (!checkout.after(checkin)){
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            }else {
+                reservation.updateDates(checkin, checkout);
+                System.out.println("Reservation: "+reservation);
+            }
+
         }
 
         sc.close();
